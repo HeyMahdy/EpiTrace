0cc7f2b5d631b70b3777923b7c0e94c0efa90185
Shahdidul <theshahidkhan.2004@gmail.com>
2026-02-14 00:49:11 +0600

    Register and Login done

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import express from "express";
 import healthRouter from "./api/routes/health.js";
+import authRouter from "./api/routes/auth.js";
 
 const app = express();
 
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 8080;
 app.use(express.json());
 
 app.use("/", healthRouter);
+app.use("/auth", authRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
