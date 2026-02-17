6d0ba6b178c4e10608efc268e66d1585b9d30321
Shahdidul <theshahidkhan.2004@gmail.com>
2026-02-18 03:15:21 +0600

    fixed validators

--- server/src/api/validators/alert.js
+++ server/src/api/validators/alert.js
@@ -1,11 +1,11 @@
 import { z } from "zod";
 
 export const sendAlertSchema = z.object({
-  monitorId: z.string().uuid(),
-  url: z.string().url(),
-  repo_link: z.string().url().optional(),
+  monitorId: z.uuid(),
+  url: z.url(),
+  repo_link: z.url().optional(),
   status: z.enum(["UP", "DOWN"]),
   error_message: z.string().optional(),
   status_code: z.number().optional(),
-  timestamp: z.string().datetime().optional(),
+  timestamp: z.string().iso.datetime().optional(),
 });
